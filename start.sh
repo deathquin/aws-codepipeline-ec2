8805c3fdd20d87ad188f1e0896ddb3e8dc1f891e
deathquin <roehd007>
2020-07-01 18:24:08 +0900

    codepipeline info change

--- start.sh
+++ start.sh
@@ -7,5 +7,6 @@ export NVM_DIR="$HOME/.nvm"
 cd $PROJECT_ROOT
 
 # 원래 node 프로세스 종료
-sudo kill -9 `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
+#sudo killall -9 ps -ef | grep 'node bin/www' | awk '{print $2}'
+forever start bin/www
 nohup npm start >/home/ubuntu/logs 2>&1 </home/ubuntu/errors &
