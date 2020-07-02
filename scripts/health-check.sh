56ec9cb5899287d6039897eca6b99361086c05e2
deathquin <deathquin@gmail.com>
2020-07-02 18:21:20 +0900

    npm-install 권한 추가

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -1,3 +1,5 @@
 # /healthCheck.sh
 # curl "http://localhost/healthcheck"
-nc -zv 127.0.0.1 80
+# nc -zv 127.0.0.1 80
+# curl -sSf http://localhost:80/healthcheck > /dev/null
+sudo curl -Is http://localhost:80/healthcheck | head -n 1
