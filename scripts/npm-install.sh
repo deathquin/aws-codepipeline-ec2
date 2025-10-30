f1a0ca031c1d31b993b1bee15b8c8c65833bc062
deathquin <deathquin@gmail.com>
2025-10-30 17:39:57 +0900

    포트 변경 2

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-if  if [ -d "/home/ubuntu/build" ]; then sudo rm -Rf "/home/ubuntu/build"; fi # [ -d "/home/ec2-user" ]; then sudo rm -Rf "/home/ec2-user"; fi #
 cd /home/ubuntu/build # /home/ubuntu/build - ubuntu /home/ec2-user/
 sudo npm install
 sudo npm install -g forever
