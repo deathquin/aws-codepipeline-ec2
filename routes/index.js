5b41e6ed7659bad7ffb66d15105bed08d2e5e451
deathquin <deathquin@gmail.com>
2025-10-30 18:04:52 +0900

    포트를 변경하고 메인 내용을 변경

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', function(req, res, next) {
   console.log("hello");
   //res.render('index', { title: 'Express' });
-  res.json("Node.js Framework Express Go 1 ");
+  res.json("Node.js Framework Express Go 12 ");
 });
 
 router.get('/healthcheck', function(req, res, next) {
