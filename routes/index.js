75d2be257f1e563a16744da988274135353ff461
deathquin <deathquin@gmail.com>
2025-10-30 17:48:34 +0900

    포트 변경 2

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', function(req, res, next) {
   console.log("hello");
   //res.render('index', { title: 'Express' });
-  res.json("Node.js Framework Express Go 123");
+  res.json("Node.js Framework Express Go 12345");
 });
 
 router.get('/healthcheck', function(req, res, next) {
