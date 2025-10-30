4050f4bdc648b2674a7c9c1555867b7b9d6e46ce
deathquin <deathquin@gmail.com>
2025-10-30 17:01:15 +0900

    포트 변경 2

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-forever start /server/aws-codepipeline-ec2/bin/www # /home/ubuntu/build/bin/www - ubuntu, /home/ec2-user/bin/www
+forever start /home/ubuntu/build/bin/www # /home/ubuntu/build/bin/www - ubuntu, /home/ec2-user/bin/www
 nohup npm start >/home/logs 2>&1 </home/errors & # /home/ubuntu - ubuntu
